477d836d91c555bd2bf8564b129c558da20b8118
akapellla <alexnevod12@gmail.com>
2026-02-09 17:43:43 +0300

    Initialize project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./main.scss";
+import Header from "./components/Header/";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
